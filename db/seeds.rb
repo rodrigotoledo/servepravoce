76f5badff8c8711f7699f79926c12b2debcc1d4b
Rodrigo Toledo <rodrigo@Rodrigos-MacBook-Pro.local>
2022-10-19 09:38:07 -0300

    search on top

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,7 @@
 require 'faker'
 User.create!(email: Faker::Internet.email, password: 'aassdd123', password_confirmation: 'aassdd123')
 User.create!(email: 'admin@test.com', password: 'aassdd123', password_confirmation: 'aassdd123')
-
-
-10.times.each do
-  ObjectToSell.create(title: Faker::Coffee.blend_name, description: Faker::Lorem.paragraph)
+4.times.each do
+  object_to_sell = ObjectToSell.create(title: Faker::Coffee.blend_name, description: Faker::Lorem.paragraph)
+  object_to_sell.image_cover.attach(io: File.open("#{Rails.root}/app/assets/images/test.jpg"), filename: 'test.jpg')
 end
