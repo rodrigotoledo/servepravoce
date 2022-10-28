8dc2122551052b5fba7c04db1da728e69b246782
RToledo DEV <rodrigo@rtoledo.inf.br>
2022-10-28 16:17:17 -0300

    turbo stream - main section responsive - filter with objects to sell

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 
 Rails.application.routes.draw do
   root to: 'welcome#index'
-  get 'welcome/index'
+  post '/search', to: 'welcome#search', as: :search
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
